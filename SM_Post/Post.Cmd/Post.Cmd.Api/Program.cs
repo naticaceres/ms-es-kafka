67fcc35cf59d8a38c5c5d4e8570ee12c7c86df92
Nati <natalia@savvyy.ai>
2023-10-17 17:12:22 -0400

    35 36- CommandHandler, mediator pattern colleagues

--- SM_Post/Post.Cmd/Post.Cmd.Api/Program.cs
+++ SM_Post/Post.Cmd/Post.Cmd.Api/Program.cs
@@ -1,15 +1,22 @@
 using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
 using CQRS.Core.Infrastructure;
+using Post.Cmd.Api.Commands;
+using Post.Cmd.Domain.Aggregate;
 using Post.Cmd.Infrastructure.Config;
+using Post.Cmd.Infrastructure.Handlers;
 using Post.Cmd.Infrastructure.Repositories;
 using Post.Cmd.Infrastructure.Stores;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+// the order in these is important, the last ones depend on the first ones orderly.
 builder.Services.Configure<MongoDbConfig>(builder.Configuration.GetSection(nameof(MongoDbConfig)));
 builder.Services.AddScoped<IEventStoreRepository, EventStoreRepository>();
 builder.Services.AddScoped<IEventStore, EventStore>();
+builder.Services.AddScoped<IEventSourcingHandler<PostAggregate>, EventSourcingHandler>();
+builder.Services.AddScoped<ICommandHandler, CommandHandler>();
 
 
 builder.Services.AddControllers();
