8012203b13736b58964263e7fc4757f2101efc5e
Nati <natalia@savvyy.ai>
2023-10-16 22:34:21 -0400

    32 33- IEventSourcingHandler EventSourcingHandler

---
+++ SM_Post/Post.Cmd/Post.Cmd.Infrastructure/Handlers/EventSourcingHandler.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CQRS.Core.Domain;
+using CQRS.Core.Handlers;
+using CQRS.Core.Infrastructure;
+using Post.Cmd.Domain.Aggregate;
+
+namespace Post.Cmd.Infrastructure.Handlers
+{
+    public class EventSourcingHandler : IEventSourcingHandler<PostAggregate>
+    {
+        private readonly IEventStore _eventStore;
+
+        public EventSourcingHandler(IEventStore eventStore)
+        {
+            _eventStore = eventStore;
+        }
+        public async Task<PostAggregate> GetByIdAsync(Guid aggregateId)
+        {
+            var aggregate = new PostAggregate();
+            var events = await _eventStore.GetEventAsync(aggregateId);
+
+            if (events == null || !events.Any()) return aggregate;
+
+            aggregate.ReplayEvents(events);
+            var latestVersion = events.Select(x => x.Version).Max();
+            aggregate.Version = latestVersion;
+
+            return aggregate;
+        }
+
+        public async Task SaveAsync(AggregateRoot aggregate)
+        {
+            await _eventStore.SaveEventAsync(aggregate.Id, aggregate.GetUncommittedChanges(), aggregate.Version);
+            aggregate.MarkChangesAsCommitted();
+        }
+    }
+}
