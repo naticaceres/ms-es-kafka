c2f8bb4e69017c865dd70b88a387a9f2707d8ea4
Nati <natalia@savvyy.ai>
2023-10-17 22:24:41 -0400

    48- PostRepository

--- SM_Post/Post.Query/Post.Query.Domain/Repositories/IPostRepository.cs
+++ SM_Post/Post.Query/Post.Query.Domain/Repositories/IPostRepository.cs
@@ -11,7 +11,7 @@ namespace Post.Query.Domain.Repositories
         Task<PostEntity> GetByIdAsync(Guid postId);
         Task<List<PostEntity>> ListAllAsync();
         Task<List<PostEntity>> ListByAuthorAsync(string author);
-        Task<List<PostEntity>> ListWithLikeAsync(int numberOfLiked);
+        Task<List<PostEntity>> ListWithLikeAsync(int numberOfLikes);
         Task<List<PostEntity>> ListWithCommentsAsync();
     }
 }
