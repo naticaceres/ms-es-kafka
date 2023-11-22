488822a98fc77ec17b438135e3f48c4a7cc9adb9
Nati <natalia@savvyy.ai>
2023-11-22 15:05:08 -0500

    architecture diagram and bugfix on newPostController to return ID

--- SM_Post/Post.Cmd/Post.Cmd.Api/Controllers/NewPostController.cs
+++ SM_Post/Post.Cmd/Post.Cmd.Api/Controllers/NewPostController.cs
@@ -35,6 +35,7 @@ namespace Post.Cmd.Api.Controllers
 
                 return StatusCode(StatusCodes.Status201Created, new NewPostResponse
                 {
+                    Id = id,
                     Message = "New post creation request completed successfully!"
                 });
             }
